809ef26064b560ce8ce41e07123497b47e148ffb
martiandreamer <martiandreamer@mail.com>
2024-04-03 22:13:22 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import { Theme } from "./constants";
+import { LeftNavigator } from "./components/LeftNavigator";
+
+export const ThemeContext = createContext();
 
 function App() {
+  const  [theme, setTheme] = useState(Theme.LIGHT);
+
+  const css = {
+    fontFamily: "JetBrains Mono",
+    fontSize: "1em",
+    padding: "0px",
+    margin: "0px",
+    display: "flex",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div style={css} className="App">
+        <LeftNavigator>Hello</LeftNavigator>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const Theme = {
+  LIGHT: {
+    color1: "#99C2FF",
+    color2: "#E6EFFF",
+    color3: "#CCDFFF",
+    textColor: "#0E0113"
+  },
+  DARK: {
+    color1: "#0E0113",
+    color2: "#252336",
+    color3: "#454266",
+    textColor: "#E6EFFF"
+  },
+  switch(theme) {
+    if (theme === Theme.DARK) {
+      return Theme.LIGHT;
+    } else {
+      return Theme.DARK;
+    }
+  },
+};
